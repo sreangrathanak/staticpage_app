fd9ea51dfa6215cf2a56496acf4b91ac4c78d842
SreangRathanak <sreangrathanak@yahoo.com>
2014-10-09 11:20:20 +0700

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   end
   def show
 	@user=User.find(params[:id])  	
+  @microposts= @user.microposts.paginate(page: params[:page])
 	#debugger
   end
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+             email: "samkimly@yahoo.com",
+             password:              "1234567890",
+             password_confirmation: "1234567890",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
@@ -16,4 +16,9 @@ User.create!(name:  "Example User",
                password_confirmation: password,
                activated: true,
               activated_at: Time.zone.now)
+end
+users=User.order(:created_at).take(6)
+50.times do
+  content= Faker::Lorem.sentence(5)
+  users.each{|user| user.microposts.create!(content: content)}
 end
